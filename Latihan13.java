5957122877c6f9f80568504c3bc7a9aa619cbe44
hauraarchia <hauraarchia@gmail.com>
2023-09-05 18:54:59 +0700

    coba tugas

---
+++ Latihan13.java
@@ -0,0 +1,18 @@
+/**
+ * Latihan13
+ */
+public class Latihan13 {
+
+    public static void main(String[] args) {
+        System.out.println("Nama Saya Haura");
+        System.out.println("\nSaya adalah mahasiswa Teknologi Informasi-POLINEMA");
+   
+        System.out.println("***** IDENTITAS DIRI *****");
+        System.out.println("Nama   :Haura archia");
+        System.out.println("Absen  :13");
+        System.out.println("Kelas  :1");
+        System.out.println("Prodi  :D4 SIB");
+        System.out.println("Jurusan:Teknologi Informasi");
+       
+    }
+}
