754b8577020d5db6b25b8de74a78dd2c4adc5b0b
hauraarchia <hauraarchia@gmail.com>
2023-10-04 10:57:44 +0700

    Job5-01

---
+++ praktikum05/PemilihanBilangan13.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+/**
+ * PemilihanBilangan
+ */
+public class PemilihanBilangan13 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Masukkan sebuah Angka: ");;
+        int angka = sc.nextInt();
+
+        if (angka % 2 == 0)
+    {
+        System.out.println("Angka " + angka + " termasuk bilangan genap");
+    }
+    else{
+        System.out.println("Angka " + angka + " termasuk bilangan ganjil");;
+    }
+    }
+}
