4a46d5a2f516275a2b69f44adfd66f8e24b9c06e
hauraarchia <hauraarchia@gmail.com>
2023-10-04 11:58:10 +0700

    menggunakan ternary operator

--- praktikum05/PemilihanBilangan13.java
+++ praktikum05/PemilihanBilangan13.java
@@ -8,13 +8,16 @@ public class PemilihanBilangan13 {
         Scanner sc = new Scanner(System.in);
         System.out.println("Masukkan sebuah Angka: ");;
         int angka = sc.nextInt();
-
-        if (angka % 2 == 0)
-    {
-        System.out.println("Angka " + angka + " termasuk bilangan genap");
-    }
-    else{
-        System.out.println("Angka " + angka + " termasuk bilangan ganjil");;
-    }
+//ternary operator haura13
+        String hasil;
+        hasil = (angka % 2 == 0) ? "bilangan genap" : "bilangan ganjil";
+        System.out.println(angka + " adalah " + hasil);
+    //     if (angka % 2 == 0)
+    // {
+    //     System.out.println("Angka " + angka + " termasuk bilangan genap");
+    // }
+    // else{
+    //     System.out.println("Angka " + angka + " termasuk bilangan ganjil");;
+    // }
     }
 }
