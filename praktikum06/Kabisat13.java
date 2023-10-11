fd661817515ba63c87bdcf0aaab4414c4f5f1320
hauraarchia <hauraarchia@gmail.com>
2023-10-11 13:23:30 +0700

    kabisat baru

--- praktikum06/Kabisat13.java
+++ praktikum06/Kabisat13.java
@@ -6,7 +6,7 @@ public class Kabisat13 {
         
         System.out.println("masukkan tahun : ");
         tahun = sc.nextInt();
-        if((tahun % 4) == 0 && (tahun % 100) != 0){
+        if((tahun % 4) == 0 && (tahun % 100) != 0 || ((tahun % 400) == 0)){
             System.out.println("Tahun Kabisat");
         }else{
             System.out.println("Bukan Tahun Kabisat");
