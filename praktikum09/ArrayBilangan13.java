4305cc24076214190ee36cae18874ab2545a6417
hauraarchia <hauraarchia@gmail.com>
2023-11-01 12:16:27 +0700

    array bilangan tugas

--- praktikum09/ArrayBilangan13.java
+++ praktikum09/ArrayBilangan13.java
@@ -9,10 +9,9 @@ public class ArrayBilangan13 {
         bil[2] = -7;
         bil[3] = 17;
 
-        System.out.println(bil[0]);
-        System.out.println(bil[1]);
-        System.out.println(bil[2]);
-        System.out.println(bil[3]);
+       for (int i = 0; i <= 4; i++) {
+         System.out.println(bil[i]);
+       }
     }
     
 }
