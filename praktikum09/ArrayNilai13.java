1356028a1b600c40ce197449a0fcd2257f95f833
hauraarchia <hauraarchia@gmail.com>
2023-11-01 13:23:24 +0700

    lulus dan tidak lulus

--- praktikum09/ArrayNilai13.java
+++ praktikum09/ArrayNilai13.java
@@ -5,13 +5,22 @@ public class ArrayNilai13 {
 
         int[] nilaiAkhir = new int[10];
 
-        for (int i = 0; i < 10; i++){
-            System.out.println("Masukkan Nilai akhir Mahasiswa ke-" + (i+1) + ": ");
+        // for (int i = 0; i < 10; i++){
+        //     System.out.println("Masukkan Nilai akhir Mahasiswa ke-" + (i+1) + ": ");
+        //     nilaiAkhir[i] = sc.nextInt();
+        // }
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            System.out.println("Masukkan nilai akhir Mahasiswa ke-" + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
+            
         }
         for (int i = 0; i < 10; i++) {
-            System.out.println("Nilai akhir Mahasiswa ke-" + (i+1) + " adalah " + nilaiAkhir[i]);
-            
+            if (nilaiAkhir[i] > 70) {
+                System.out.println("Mahasiswa ke-" + (i+1) + " lulus");    
+            }else{
+                System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus");
+            }
+            // System.out.println("Nilai akhir Mahasiswa ke-" + (i+1) + " adalah " + nilaiAkhir[i]);
         }
     }
 }
