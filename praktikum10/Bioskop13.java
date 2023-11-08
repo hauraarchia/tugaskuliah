bf8ad91e002c1c4e2cc42012830bf08af5f7f0c0
hauraarchia <hauraarchia@gmail.com>
2023-11-08 13:43:25 +0700

    bisokop perulangan penonton

--- praktikum10/Bioskop13.java
+++ praktikum10/Bioskop13.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Bioskop13
  */
@@ -14,17 +16,19 @@ public class Bioskop13 {
         penonton[3][0] = "Gisel";
         penonton[3][1] = "Hana";
 
-        System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
-        System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
-        System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
-        System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println(Arrays.toString(penonton[i]));
+            
+        }
+        // System.out.printf("%s \t %s \n", penonton[0][0], penonton[0][1]);
+        // System.out.printf("%s \t %s \n", penonton[1][0], penonton[1][1]);
+        // System.out.printf("%s \t %s \n", penonton[2][0], penonton[2][1]);
+        // System.out.printf("%s \t %s \n", penonton[3][0], penonton[3][1]);
         
         System.out.println(penonton.length);
 
-        for (int i = 0; i < penonton.length; i++) {
-            System.out.println("Panjang baris ke-" + (i+1) + ": " + penonton[i].length);
-            
+        for(String[] barisPenonton : penonton){
+            System.out.println("Panjang baris: " +barisPenonton.length);
         }
     }
 }
-
