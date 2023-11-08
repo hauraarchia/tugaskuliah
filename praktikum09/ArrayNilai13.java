d08f53eb0a52175a405a1fbf02cb53d6d3eae51c
hauraarchia <hauraarchia@gmail.com>
2023-11-08 09:42:31 +0700

    array nilai menampilkan jumlah yg lulus, rata rata, dll

--- praktikum09/ArrayNilai13.java
+++ praktikum09/ArrayNilai13.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 public class ArrayNilai13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int total = 0;
+        int max, min;
+        int jumlahLulus=0;
+        int jumlahTidakLulus=0;
 
         int[] nilaiAkhir = new int[10];
 
-        // for (int i = 0; i < 10; i++){
-        //     System.out.println("Masukkan Nilai akhir Mahasiswa ke-" + (i+1) + ": ");
-        //     nilaiAkhir[i] = sc.nextInt();
-        // }
         for (int i = 0; i < nilaiAkhir.length; i++) {
             System.out.println("Masukkan nilai akhir Mahasiswa ke-" + (i+1) + ": ");
             nilaiAkhir[i] = sc.nextInt();
-            
         }
         for (int i = 0; i < 10; i++) {
             if (nilaiAkhir[i] > 70) {
@@ -20,7 +19,31 @@ public class ArrayNilai13 {
             }else{
                 System.out.println("Mahasiswa ke-" + (i+1) + " tidak lulus");
             }
-            // System.out.println("Nilai akhir Mahasiswa ke-" + (i+1) + " adalah " + nilaiAkhir[i]);
+            total += nilaiAkhir[i];
         }
+        max = nilaiAkhir[0];
+        min = nilaiAkhir[0];
+        for (int i = 0; i < nilaiAkhir.length; i++) {
+            if (nilaiAkhir[i] > max) {
+                max = nilaiAkhir[i];    
+        }
+        if (nilaiAkhir[i] < min) {
+                min = nilaiAkhir[i];    
+        }
+        if (nilaiAkhir[i] >= 70) {
+            jumlahLulus++;
+        } else{
+            jumlahTidakLulus++;
+        }
+
     }
+        double average = total / nilaiAkhir.length;
+        System.out.println("Rata-rata: " + average);
+        System.out.println("Nilai tertinggi: " +max);
+        System.out.println("Nilai Terendah: " +min);
+        System.out.println("Jumlah mahasiswa lulus: " +jumlahLulus);
+        System.out.println("Jumlah mahasiswa tidak lulus: " +jumlahTidakLulus);
+
 }
+}
+
