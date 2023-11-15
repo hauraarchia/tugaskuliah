06f758866670e4041e8baa58a3696d9eae4fe06b
hauraarchia <hauraarchia@gmail.com>
2023-11-15 13:36:22 +0700

    triangle betul

---
+++ praktikum11/triangle13.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class triangle13 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Masukkan jumlah *: ");
+        int n = s.nextInt();
+        
+        int i = 1;
+        while (i <= n) {
+            int j = 0;
+
+            while (j < i) {
+                System.out.print("*");
+                j++;
+            }
+            
+            i++;
+            System.out.println();
+            
+        }
+    }
+}
