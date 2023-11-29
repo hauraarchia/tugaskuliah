380ad1195c164beba90dfb2164c05bc36f1bf006
hauraarchia <hauraarchia@gmail.com>
2023-11-29 13:09:55 +0700

    fungsi dg parameter selesai

---
+++ praktikum13/Welcome2.java
@@ -0,0 +1,11 @@
+public class Welcome2 {
+    public static void main(String[] args) {
+        displayWelcomeMessage("Ani");
+        displayWelcomeMessage("Budi");
+        displayWelcomeMessage("Cica");
+        displayWelcomeMessage("Dodi");
+    }
+    private static void displayWelcomeMessage(String name) {
+        System.out.println("Selamat datang di Politeknik Negeri Malang, " + name);
+    }
+}
