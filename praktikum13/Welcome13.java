ac2f4eb2111a5a3038dc963af7b6ca784a555304
hauraarchia <hauraarchia@gmail.com>
2023-11-29 10:57:10 +0700

    asli

---
+++ praktikum13/Welcome13.java
@@ -0,0 +1,14 @@
+/**
+ * Welcome13
+ */
+public class Welcome13 {
+
+    public static void main(String[] args) {
+        displayWelcomeMessage();
+        
+    }
+    public static void displayWelcomeMessage() {
+        System.out.println("Selamat Datang di Politeknik Negeri Malang");
+        
+    }
+}
